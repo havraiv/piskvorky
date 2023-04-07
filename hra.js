22319437341376a0eecec0bf09572915ce7a5f2e
Iva Havranová <havranova.i@seznam.cz>
2023-04-07 21:29:48 +0200

    Připraveno na code review

--- hra.js
+++ hra.js
@@ -1,4 +1,13 @@
 let currentPlayer = 'circle';
+const crossSvg = `<span class="whoplays__player">HRAJE: </span>
+<svg class="cross" width="20" height="20" viewBox="0 0 20 20" overflow="visible" stroke="white" stroke-width="2.5">
+  <line x2="20" y2="20" />
+  <line x1="20" y2="20" />
+</svg>`;
+const circleSvg = `<span class="whoplays__player">HRAJE: </span>
+<svg class="circle" width="36" height="36">
+  <circle class="circle" cx="18" cy="18" r="10" stroke="white" stroke-width="2.5" fill="transparent"/>
+</svg>`;
 
 const addClass = (event) => {
   if (currentPlayer === 'circle') {
@@ -7,11 +16,7 @@ const addClass = (event) => {
     const circleElm = document.querySelector('.circle');
     circleElm.remove();
     const whoPlaysElm = document.querySelector('.whoplays');
-    whoPlaysElm.innerHTML = `<span class="whoplays__player">HRAJE: </span>
-    <svg class="cross" width="20" height="20" viewBox="0 0 20 20" overflow="visible" stroke="white" stroke-width="2.5">
-      <line x2="20" y2="20" />
-      <line x1="20" y2="20" />
-    </svg>`;
+    whoPlaysElm.innerHTML = crossSvg;
     return (currentPlayer = 'cross');
   } else if (currentPlayer === 'cross') {
     event.target.classList.add('board__field--cross');
@@ -19,10 +24,7 @@ const addClass = (event) => {
     const crossElm = document.querySelector('.cross');
     crossElm.remove();
     const whoPlaysElm = document.querySelector('.whoplays');
-    whoPlaysElm.innerHTML = `<span class="whoplays__player">HRAJE: </span>
-    <svg class="circle" width="36" height="36">
-      <circle class="circle" cx="18" cy="18" r="10" stroke="white" stroke-width="2.5" fill="transparent"/>
-    </svg>`;
+    whoPlaysElm.innerHTML = circleSvg;
     return (currentPlayer = 'circle');
   }
 };
