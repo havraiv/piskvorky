849d1462fe73e2357174a1ae75560e1ba22dde42
Iva Havranová <havranova.i@seznam.cz>
2023-04-07 13:18:02 +0200

    Překlikávání ikonky u Hraje:

--- hra.js
+++ hra.js
@@ -3,9 +3,42 @@ let currentPlayer = 'circle';
 const addClass = (event) => {
   if (currentPlayer === 'circle') {
     event.target.classList.add('board__field--circle');
+    const circleElm = document.querySelector('.circle');
+    circleElm.remove();
+    const whoPlaysElm = document.querySelector('.whoplays');
+    whoPlaysElm.innerHTML = `<span class="whoplays__player">HRAJE: </span>
+    <svg class="cross"
+            xmlns="http://www.w3.org/2000/svg"
+            width="20"
+            height="20"
+            viewBox="0 0 20 20"
+            overflow="visible"
+            stroke="white"
+            stroke-width="2.5"
+          >
+            <line x2="20" y2="20" />
+            <line x1="20" y2="20" />
+          </svg>
+
+    `;
     return (currentPlayer = 'cross');
   } else if (currentPlayer === 'cross') {
     event.target.classList.add('board__field--cross');
+    const crossElm = document.querySelector('.cross');
+    crossElm.remove();
+    const whoPlaysElm = document.querySelector('.whoplays');
+    whoPlaysElm.innerHTML = `<span class="whoplays__player">HRAJE: </span>
+    <svg class="circle" width="36" height="36">
+      <circle
+        class="circle"
+        cx="18"
+        cy="18"
+        r="10"
+        stroke="white"
+        stroke-width="2.5"
+        fill="transparent"
+      />
+    </svg>`;
     return (currentPlayer = 'circle');
   }
 };
